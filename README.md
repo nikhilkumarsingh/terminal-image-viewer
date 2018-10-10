e4ff9ac5bb7c4e30206120c71ec3e55679cf1ddc
nikhilkumarsingh <nikhilksingh97@gmail.com>
2018-10-10 13:52:30 +0530

    Initial Commit

---
+++ README.md
@@ -0,0 +1,47 @@
+# Terminal Image Viewer
+
+>A Simple Python Script to Display Images in Linux Terminal
+
+
+## Resources
+
+- [Formatted text in Linux Terminal](https://indianpythonista.wordpress.com/2017/04/02/formatted-text-in-linux-terminal-using-python/)
+
+- [ANSI color palette](https://kui.github.io/ansi_color_palette/)
+
+- [ANSI Color Specific RGB Sequence Bash](https://stackoverflow.com/a/26665998/8221580)
+
+
+## Dependencies
+
+- pillow
+	
+	Install using pip:
+
+	```
+	$ pip install pillow
+	```
+
+- numpy
+
+	Install using pip:
+
+	```
+	$ pip install numpy
+	```
+
+## Usage
+
+```
+$ python img-viewer.py <image file name>
+```
+
+For example:
+
+```
+$ python img-viewer.py dog.jpg
+```
+
+## Tutorial
+
+[![](https://i.imgur.com/6dFbzca.png)](https://youtu.be/f77rKzqBzEA)
---
+++ img-viewer.py
@@ -0,0 +1,43 @@
+import sys
+import numpy as np
+from PIL import Image
+
+
+def get_ansi_color_code(r, g, b):
+    if r == g and g == b:
+        if r < 8:
+            return 16
+        if r > 248:
+            return 231
+        return round(((r - 8) / 247) * 24) + 232
+    return 16 + (36 * round(r / 255 * 5)) + (6 * round(g / 255 * 5)) + round(b / 255 * 5)
+
+
+def get_color(r, g, b):
+    return f"\x1b[48;5;{int(get_ansi_color_code(r,g,b))}m \x1b[0m"
+
+
+def show_image(img_path):
+	try:
+		img = Image.open(img_path)
+	except FileNotFoundError:
+		exit('Image not found.')
+
+	h = 100
+	w = int((img.width / img.height) * h)
+
+	img = img.resize((w,h), Image.ANTIALIAS)
+	img_arr = np.asarray(img)
+	h,w,c = img_arr.shape
+
+	for x in range(h):
+	    for y in range(w):
+	        pix = img_arr[x][y]
+	        print(get_color(pix[0], pix[1], pix[2]), sep='', end='')
+	    print()
+
+
+if __name__ == '__main__':
+	if len(sys.argv) > 1:
+		img_path = sys.argv[1]
+		show_image(img_path)
